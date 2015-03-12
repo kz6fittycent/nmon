59c262851b3bc3f34f4e04b8dfcd4b0229d95b7b
Dmitry <failsild@gmail.com>
2015-03-12 11:51:03 +0300

    build script extend of makefile

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+CFLAGS="-g -O2 -D JFS -D GETUSER -Wall -D LARGEMEM"
+LDFLAGS="-lncurses -g"
+COMPILER="gcc"
+FILE=lmon14i.c
+if [ `which $COMPILER` ]; then
+    $COMPILER -o nmon $FILE $CFLAGS $LDFLAGS -D X86
+else
+    echo "Please install gnu linux compiler or fix COMPILE variable for your system in the build script"
+fi
+
