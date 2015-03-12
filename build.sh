a3842354b068f51750938e135b0f321eceabc3a1
Dmitry <failsild@gmail.com>
2015-03-12 11:55:36 +0300

    hotfix

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 CFLAGS="-g -O2 -D JFS -D GETUSER -Wall -D LARGEMEM"
 LDFLAGS="-lncurses -g"
 COMPILER="gcc"
-FILE=lmon14i.c
+FILE="lmon14i.c"
 if [ `which $COMPILER` ]; then
     $COMPILER -o nmon $FILE $CFLAGS $LDFLAGS -D X86
 else
